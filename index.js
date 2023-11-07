f67b1213762923df753e3d0215a626603ade402b
Majedah <122641068+majowl77@users.noreply.github.com>
2023-11-07 16:58:03 +0300

    Basic Requirements done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const http = require("http");
-const PORT = '8080'
+const PORT = 8000;
 
-http
-  .createServer((req, res) => {
-    /* handle http requests */
-    /* here is the code  */
-  })
-  .listen(PORT);
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.write("Hello, World!");
+    res.end();
+  } else if (req.method === "POST" && req.url === "/") {
+    let postData = "";
+    req.setEncoding("utf8");
+    req.on("data", (data) => {
+      postData += data;
+    });
+    req.on("end", () => {
+      console.log("Received data:", postData);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Data received and logged.");
 
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
+    });
+  }
+  else if (){
+    
+  }
+});
+
+server.listen(PORT, (error) => {
+  if (error) {
+    console.log("Failed to listen server on port : 3000 ");
+  } else {
+    console.log(`Server running at http://127.0.0.1:${PORT}/`);
+  }
+});
