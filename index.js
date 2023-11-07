6b3085b57928d094f8ac033caae3818a9b8caed5
Majedah <122641068+majowl77@users.noreply.github.com>
2023-11-08 00:46:05 +0300

    final changes

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, (error) => {
   if (error) {
-    console.log("Failed to listen server on port : 3000 ");
+    console.log(`Failed to listen server on port : ${PORT} `);
   } else {
     console.log(`Server running at http://127.0.0.1:${PORT}/`);
   }
